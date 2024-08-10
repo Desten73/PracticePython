8d00dec194457663e56e96f525081a4abfa74b3c
Aleksey Sadilov <sadilov_1997@mail.ru>
2024-08-10 23:02:20 +0400

    21 Class operator overloading

--- 21_Class.py
+++ 21_Class.py
@@ -16,6 +16,64 @@ class House:
             for i in range(new_floor):
                 print(i+1)
 
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            return False
+
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            return False
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            return House(self.name, self.number_of_floors + other.number_of_floors)
+        elif isinstance(other, int):
+            return House(self.name, self.number_of_floors + other)
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+            return self
+        elif isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        else:
+            return NotImplemented
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
@@ -33,3 +91,28 @@ print(h2)
 # __len__
 print(len(h1))
 print(len(h2))
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
