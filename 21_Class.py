033378830eea1580c8b6772b386448abfc289594
Aleksey Sadilov <sadilov_1997@mail.ru>
2024-08-11 00:07:06 +0400

    21 Class __del__ __new__

--- 21_Class.py
+++ 21_Class.py
@@ -1,4 +1,13 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
     def __init__(self, name, floors):
         self.name = name
         self.number_of_floors = floors
@@ -75,44 +84,57 @@ class House:
             return NotImplemented
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
-
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+#
+#
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# # __str__
+# print(h1)
+# print(h2)
+#
+# # __len__
+# print(len(h1))
+# print(len(h2))
+#
+#
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(h1 == h2) # __eq__
+#
+# h1 = h1 + 10 # __add__
+# print(h1)
+# print(h1 == h2)
+#
+# h1 += 10 # __iadd__
+# print(h1)
+#
+# h2 = 10 + h2 # __radd__
+# print(h2)
+#
+# print(h1 > h2) # __gt__
+# print(h1 >= h2) # __ge__
+# print(h1 < h2) # __lt__
+# print(h1 <= h2) # __le__
+# print(h1 != h2) # __ne__
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-# __str__
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-# __len__
-print(len(h1))
-print(len(h2))
-
-
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
-
-print(h1)
-print(h2)
-
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(House.houses_history)
