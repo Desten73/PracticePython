76106b72748eadc7531cf7a3e56181b657213cbd
Aleksey Sadilov <sadilov_1997@mail.ru>
2024-08-27 15:13:35 +0400

    homework 25 upd

--- 25_Multiple_inheritance.py
+++ 25_Multiple_inheritance.py
@@ -1,24 +1,20 @@
 class Horse:
-    def __init__(self):
-        self._x_distance = 0
-        self._sound = "Frrr"
-        super().__init__()
+    _x_distance = 0
+    _sound = "Frrr"
 
     def run(self, dx):
         self._x_distance += dx
 
 
 class Eagle:
-    def __init__(self):
-        self._y_distance = 0
-        self._sound = "I train, eat, sleep, and repeat"
-        super().__init__()
+    _y_distance = 0
+    _sound = "I train, eat, sleep, and repeat"
 
     def fly(self, dy):
         self._y_distance += dy
 
 
-class Pegasus(Horse, Eagle):
+class Pegasus(Eagle, Horse):
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
