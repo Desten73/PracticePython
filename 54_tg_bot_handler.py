0a70769a8e99d48b382d3a99a84e6e8190daa9d0
Desten73 <sadilov_1997@mail.ru>
2024-10-31 20:50:34 +0300

    54 tg bot handler

---
+++ 54_tg_bot_handler.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram import F
+from aiogram.fsm.storage import memory
+
+api = open("bot_api.txt", "r").read()
+bot = Bot(token=api, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot_memory = memory.MemoryStorage()
+dp = Dispatcher(storage=bot_memory)
+
+
+@dp.message(CommandStart())
+async def start(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message(F.text == "test")
+async def all_massages(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
