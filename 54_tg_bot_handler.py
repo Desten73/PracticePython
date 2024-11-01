68ce70e5a905e68b3faa5fcc2ad46f6cd42ea4fd
Desten73 <sadilov_1997@mail.ru>
2024-11-01 17:15:35 +0300

    54 he answer me!

--- 54_tg_bot_handler.py
+++ 54_tg_bot_handler.py
@@ -13,12 +13,12 @@ dp = Dispatcher(storage=bot_memory)
 
 @dp.message(CommandStart())
 async def start(message):
-    print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
 
 
 @dp.message(F.text)
 async def all_massages(message):
-    print("Введите команду /start, чтобы начать общение.")
+    await message.answer("Введите команду /start, чтобы начать общение.")
 
 if __name__ == "__main__":
     dp.run_polling(bot)
