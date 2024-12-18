6be18ad4ca294172588f381a856b7b2808b8bdc4
Aleksey Sadilov <sadilov_1997@mail.ru>
2024-12-18 12:48:11 +0400

    56 simulate work with db

--- 56_fast_api.py
+++ 56_fast_api.py
@@ -1,26 +1,51 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from typing import Annotated
 
 app = FastAPI()
+users = {1: 'Имя: Example, возраст: 18'}
 
 
 @app.get("/")
-async def main_page() -> dict:
-    return {"message": "Главная страница"}
+async def main_page() -> str:
+    return "Главная страница"
 
 
 @app.get("/user/admin")
-async def user_admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def user_admin() -> str:
+    return "Вы вошли как администратор"
 
 
-@app.get("/user/{user_id}")
-async def user_id(user_id: Annotated[int, Path(ge=1, le=100, description="Enter user ID", example=1)]) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+@app.get("/users")
+async def get_users() -> dict:
+    return users
 
 
-@app.get("/user/{username}/{age}")
-async def user_info(
+@app.delete("/user/{user_id}")
+async def delete_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter user ID", example=1)]) -> str:
+    if users[user_id]:
+        users.pop(user_id)
+        return f"The user {user_id} is deleted"
+    else:
+        raise HTTPException(status_code=404, detail="id not found")
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter user id", example=1)],
         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
-        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> str:
+    if users[user_id]:
+        users[user_id] = f"Имя: {username}, возраст: {age}"
+        return f"The user {user_id} is updated"
+    else:
+        raise HTTPException(status_code=404, detail="id not found")
+
+
+@app.post("/user/{username}/{age}")
+async def add_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> str:
+    user_id = int(max(users.keys()) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
